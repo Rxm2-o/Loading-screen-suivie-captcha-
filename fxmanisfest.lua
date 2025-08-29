3f55e1bd6fb6d6ae39bf4f2ea89ca6d8db042e09
Rxm×. <gautier.klein5@gmail.com>
2025-08-29 11:18:24 +0200

    fxmanisfest.lua

---
+++ fxmanisfest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'Rxm'
+description 'Presencz for fivem server'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_script{
+    'rich.lua'
+}
